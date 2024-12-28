0ab09a06c6005bc74a8cc08cc8e164f4c0f57bf9
Bakhodirbekov <otabekotabek1661@gmail.com>
2024-12-29 01:06:26 +0500

    ssss

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -7,8 +7,7 @@
     </div>
     <div v-for="project in projects" :key="project.id"
       class="bg-gray-900 text-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition duration-300 ease-in-out">
-      <img :src="project.image" alt="Project Image" class="w-full h-48 object-cover rounded-lg" />
-
+      <img :src="project.image" :alt="project.title" class="w-full h-40 object-cover" />
       <h2 class="text-xl font-semibold mt-4">{{ project.companyName }}</h2>
       <p class="mt-2 text-gray-400">{{ project.description }}</p>
 
@@ -38,166 +37,23 @@
 
 
 <script>
-export default {
-  name: "ProjectCard",
-  data() {
-    return {
-      projects: [
-        {
-          id: 1,
-          image: "/src/assets/img/milliyavto.jpg",
-          companyName: "MilliyAvto",
-          description: "Web-Sayt qilib berganmiz",
-          startDate: "2017-05-01",
-          endDate: "2019-06-01",
-          instagram: "https://www.instagram.com/milliy_avto_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          web: "https://milliyavto.uz",
+export const projects = [
+  {
+    id: 1,
+    image: require('@/assets/smm-img/ciber.png'), // Resolves image during build
+    title: 'Cyber Security',
+    description: 'Cyber Security of the Republic of Uzbekistan',
+    link: '/projects',
+  },
+  {
+    id: 2,
+    image: require('@/assets/smm-img/other.png'),
+    title: 'Another Project',
+    description: 'Description of another project',
+    link: '/projects',
+  },
+];
 
-          showDetails: false,
-        },
-        {
-          id: 2,
-          image: "/src/assets/img/tuo.png",
-          companyName: "TUO-Motors",
-          description: "SMM va Web-Ilovalar ",
-          startDate: "2022-04-10",
-          endDate: "2025-12-09",
-          instagram: "https://www.instagram.com/tuomotors.official?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          web: "https://tuo-motors.netlify.app/",
-          showDetails: false,
-        },
-        {
-          id: 3,
-          image: "/src/assets/img/toshInevst.png",
-          companyName: "Toshkent-Invest",
-          description: "Web-Sayt va E-Auksion API",
-          startDate: "2022-04-10",
-          endDate: "2028-04-10",
-          instagram: "https://www.instagram.com/toshkentinvest.uz?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          web: "projects.toshkentinvest.uz",
-          showDetails: false,
-        },
-        {
-          id: 4,
-          image: "/src/assets/img/gl.png",
-          companyName: "GL-Motors",
-          description: "Smm,Marketing,Analitika",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/glmotors_uz?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          linkedin: "gl-motors.uz",
-          showDetails: false,
-        },
-        {
-          id: 5,
-          image: "/src/assets/img/insof-bilan.png",
-          companyName: "Insof-Bilan",
-          description: "Bizni kichik project",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/insof_bilan?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          showDetails: false,
-        },
-        {
-          id: 6,
-          image: "/src/assets/img/a7-hostel.png",
-          companyName: "A7-Hostel",
-          description: "Mexmonxona web-sayt",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/a7_hostel_n1?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          web: "http://hostell.uz/",
-          github: "https://github.com/portfolio2",
-          showDetails: false,
-        },
-        {
-          id: 7,
-          image: "/src/assets/img/openshop.png",
-          companyName: "OpenShop",
-          description: "Online Market",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/openshop_uz?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          linkedin: "https://www.openshop.uz",
-          showDetails: false,
-        },
-        {
-          id: 8,
-          image: "/src/assets/img/nge.png",
-          companyName: "NGE-Motors",
-          description: "Marketing Analitika Smm",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/nge_auto?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          showDetails: false,
-        },
-        {
-          id: 9,
-          image: "/src/assets/img/zty.png",
-          companyName: "ZTY Car Energy",
-          description: "Brend-In Market-In Analitik Smm",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/zty_uz?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          web: "https://t.me/ztycarenergy",
-          showDetails: false,
-        },
-        {
-          id: 10,
-          image: "/src/assets/img/elq2.png",
-          companyName: "ELQ Banking System",
-          description: "Web App",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          web: "https://elqbanking.uz/",
-          showDetails: false,
-        },
-        {
-          id: 11,
-          image: "/src/assets/img/anvar-a.png",
-          companyName: "Anvar Avto-Kredit",
-          description: "SMM Marketing",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/anvar_avto?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          showDetails: false,
-        },
-        {
-          id: 12,
-          image: "/src/assets/img/olam-avto.png",
-          companyName: "Olam Avto-Kredit",
-          description: "SMM Marketing",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/olamavto_uz?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          showDetails: false,
-        },
-        {
-          id: 13,
-          image: "/src/assets/img/elpar.png",
-          companyName: "Elparvar Avto-Kredit",
-          description: "SMM Marketing",
-          startDate: "2022-04-10",
-          endDate: "2022-12-15",
-          instagram: "https://www.instagram.com/elparvar_avtokredit?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-          showDetails: false,
-        },
-      ],
-    };
-  },
-  methods: {
-    toggleDetails(projectId) {
-      // Faqat bitta loyiha uchun batafsil ma'lumotni ochish yoki yopish
-      this.projects.forEach((project) => {
-        if (project.id === projectId) {
-          project.showDetails = !project.showDetails;
-        } else {
-          project.showDetails = false;
-        }
-      });
-    },
-  },
-};
 </script>
 
 <style scoped>
